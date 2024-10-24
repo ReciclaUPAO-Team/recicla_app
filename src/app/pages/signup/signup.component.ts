f7d20774ad3b9093c0c0c600f523b24dafb0fe56
DinoBudino <zahir_mauricio@outlook.es>
2024-10-24 05:59:37 -0500

    Registro de usuario ahora refresca la pagian y redirige al login

--- src/app/pages/signup/signup.component.ts
+++ src/app/pages/signup/signup.component.ts
@@ -1,4 +1,4 @@
-
+import { Router } from '@angular/router';
 import { MatSnackBar } from '@angular/material/snack-bar';
 import { UserService } from './../../service/user.service';
 import { Component, OnInit } from '@angular/core';
@@ -21,26 +21,40 @@ export class SignupComponent implements OnInit {
     dni:'',
   }
 
-  constructor(private userService:UserService,private snack:MatSnackBar) { }
+  constructor(private userService:UserService,private snack:MatSnackBar, private router:Router) { }
 
   ngOnInit(): void {
   }
 
-  formSubmit(){
+  formSubmit() {
     console.log(this.user);
-    if(this.user.username == '' || this.user.username == null){
-      this.snack.open('El nombre de usuario es requerido !!','Aceptar',{
-        duration : 3000,
-        verticalPosition : 'top',
-        horizontalPosition : 'right'
+    if (this.user.username == '' || this.user.username == null) {
+      this.snack.open('El nombre de usuario es requerido !!', 'Aceptar', {
+        duration: 3000,
+        verticalPosition: 'top',
+        horizontalPosition: 'right'
       });
       return;
     }
+
+    // Registra el usuario
     this.userService.addUsuario(this.user).subscribe(
       (data) => {
         console.log(data);
-        Swal.fire('Usuario guardado','Usuario registrado con exito en el sistema','success');
-      },(error) => {
+
+        // Muestra la alerta y espera que el usuario interactúe
+        Swal.fire({
+          title: 'Usuario guardado',
+          text: 'Usuario registrado con éxito en el sistema',
+          icon: 'success',
+          confirmButtonText: 'Aceptar'
+        }).then((result) => {
+          if (result.isConfirmed) {
+            // Redirige al componente de login después de que el usuario cierre el Swal
+            window.location.href = '/login';
+          }
+        });
+      }, (error) => {
         console.log(error);
         let errorMessage = 'Ha ocurrido un error en el sistema !!';
         if (error.error && error.error.message) {
@@ -50,9 +64,6 @@ export class SignupComponent implements OnInit {
           duration: 3000
         });
       }
-      
-    )
+    );
   }
-
-  
 }
